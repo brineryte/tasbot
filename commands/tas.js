ddfc1e4edaf2a397db0abbd52c9bc4d98c043582
Brian Wright <bwright@webstaurantstore.com>
2021-02-11 15:26:23 -0500

    first commit

---
+++ commands/tas.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'tas',
+    description: 'main command for retrieving lyrics',
+    execute(message, args){
+        message.channel.send('If I ruled the world...')
+    }
+}
