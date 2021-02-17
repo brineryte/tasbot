3a1a70fb95e666f5b84b5a9301f504b53b576802
Brian Wright <bwright@webstaurantstore.com>
2021-02-16 23:41:47 -0500

    got tas command closer to final product - works but just returns all lyrics of any song from 3750

--- commands/tas.js
+++ commands/tas.js
@@ -1,7 +1,22 @@
+const { randomInt } = require('crypto');
+const fs = require('fs');
+
 module.exports = {
     name: 'tas',
     description: 'main command for retrieving lyrics',
-    execute(message, args){
-        message.channel.send('If I ruled the world...')
+    execute(message, args) {
+
+        const albumDict = [];
+
+        files = fs.readdirSync('./lyrics');
+
+        files.forEach( (file) => {
+            var contents = fs.readFileSync('./lyrics/' + file, 'utf8');
+            albumDict.push(JSON.parse(contents));
+        });
+
+        let album = albumDict[0]
+        let song = album[Math.floor(Math.random() * album.length)]
+        message.channel.send(">>> ```" + song.lyrics + "```");
     }
 }
